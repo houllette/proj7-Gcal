9dfea2a6cc272ee531192c642bec222bc627e31e
Holden Oullette <holdenoullette@gmail.com>
2016-11-18 15:28:42 -0800

    cleaned up code a little bit

--- flask_main.py
+++ flask_main.py
@@ -348,48 +348,6 @@ def list_calendars(service):
             "primary": primary
             })
     return sorted(result, key=cal_sort_key)
-'''
-def list_events(service, selected_calendars):
-    """
-    Given a google 'service' object and list of selected calendars, return a list of
-    events from the selected calendars within the submitted date range.
-    Each event is represented by a dict.
-    """
-    app.logger.debug("Entering list_events")
-    page_token = None
-    result = [ ]
-    for cal_id in selected_calendars:
-        while True:
-          events_list = service.events().list(calendarId=cal_id, pageToken=page_token, timeMin=flask.session["begin_date"], timeMax=flask.session["end_date"]).execute()
-          for event in events_list["items"]:
-            if "summary" in event:
-                if 'transparency' not in event:
-                    if 'description' in event:
-                        desc = event['description']
-                    else:
-                        desc = '(no description)'
-
-                    if 'date' in event['start']:
-                        start_date = "ALL DAY"
-                    else:
-                        start_date = event['start']['dateTime']
-
-                    if 'date' in event['end']:
-                        end_date = "ALL DAY"
-                    else:
-                        end_date = event['end']['dateTime']
-
-                    result.append({
-                    'summary': event['summary'],
-                    'desc': desc,
-                    'start_date': start_date,
-                    'end_date': end_date
-                    })
-          page_token = events_list.get("nextPageToken")
-          if not page_token:
-            break
-    return result
-'''
 
 def cal_sort_key( cal ):
     """
@@ -406,32 +364,6 @@ def cal_sort_key( cal ):
     else:
        primary_key = "X"
     return (primary_key, selected_key, cal["summary"])
-'''
-def conflicting_events(events):
-
-    Given a list of events, checks against user inputted time frame
-    and returns list of events that fall between the requested time frame
-
-    conflict = [ ]
-    for event in events:
-        if event['start_date'] == "ALL DAY" or event['end_date'] == "ALL DAY":
-            conflict.append(event)
-        else:
-            event_start = arrow.get(event['start_date'].split('T')[1][0:8], 'HH:mm:ss')
-            event_end = arrow.get(event['end_date'].split('T')[1][0:8], 'HH:mm:ss')
-            request_start = arrow.get(flask.session['begin_time'].split('T')[1][0:8], 'HH:mm:ss')
-            request_end = arrow.get(flask.session['end_time'].split('T')[1][0:8], 'HH:mm:ss')
-            #starts within pre-existing event
-            if event_start < request_start and request_start < event_end:
-                conflict.append(event)
-            #ends within pre-existing event
-            elif event_start < request_end and request_end < event_end:
-                conflict.append(event)
-            #pre-existing event happens within request
-            elif request_start < event_start and event_end < request_end:
-                conflict.append(event)
-    return conflict
-'''
 
 #################
 #
